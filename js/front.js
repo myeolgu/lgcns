3e174d9b0773bd9d74037ccb4e077347a9fcf639
leejuyeop <juyeop198@etribe.co.kr>
2023-02-23 15:08:37 +0900

    commit_04

--- js/front.js
+++ js/front.js
@@ -1,7 +1,12 @@
+
+
 $(document).ready(function () {
   let mySVG = $('.svg-draw').drawsvg();
   mySVG.drawsvg('animate');
 
   let mySVG2 = $('.svg-draw-02').drawsvg();
   mySVG2.drawsvg('animate');
+
+  // let test = $('.svg-draw').offset().top;
+  // console.log(test);
 });
